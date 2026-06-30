ad9bba316b76a1b6c7530eb92288aecd274e10b2
gauravgupta745 <gaurav.guptagn4@gmail.com>
2026-06-30 14:51:39 +0530

    my first commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "resource_groups1" {
+  name     = "gauravaction-rg1"
+  location = "centralindia"
+}
+resource "azurerm_resource_group" "resource_groups2" {
+  name     = "gauravaction-rg2"
+  location = "centralindia"
+}
+resource "azurerm_resource_group" "resource_groups3" {
+  name     = "gauravaction-rg3"
+  location = "centralindia"
+}
+resource "azurerm_resource_group" "resource_groups4" {
+  name     = "gauravaction-rg4"
+  location = "centralindia"
+}
